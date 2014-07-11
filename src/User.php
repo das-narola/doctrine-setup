677d3e9004ec9a799661c1810de51b9af34284a0
unknown <sdarshan@c18.narola.local>
2014-07-11 15:30:44 +0530

    first commit on it. Adding all files.

---
+++ src/User.php
@@ -0,0 +1,57 @@
+<?php
+// src/User.php
+/**
+ * @Entity @Table(name="users")
+ */
+
+use Doctrine\Common\Collections\ArrayCollection; 
+
+class User
+{
+    /**
+     * @Id @GeneratedValue @Column(type="integer")
+     * @var int
+     */
+    protected $id;
+    /**
+     * @Column(type="string")
+     * @var string
+     */
+    protected $name;
+	
+	protected $reportedBugs = null;
+    protected $assignedBugs = null;
+
+    public function __construct()
+    {
+        $this->reportedBugs = new ArrayCollection();
+        $this->assignedBugs = new ArrayCollection();
+    }
+	
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+	
+	
+
+    public function addReportedBug($bug)
+    {
+        $this->reportedBugs[] = $bug;
+    }
+
+    public function assignedToBug($bug)
+    {
+        $this->assignedBugs[] = $bug;
+    }
+}
